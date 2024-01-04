35bbbb4aea30a4dec6f1f3674096c992bc280636
Kevin Vicente <kcvicente@utpl.edu.ec>
2024-01-04 17:33:59 -0500

    cambios

--- ejercicios/Problemas/src/problemas/Problema2.java
+++ ejercicios/Problemas/src/problemas/Problema2.java
@@ -5,6 +5,9 @@
  */
 package problemas;
 
+import java.util.Scanner;
+import java.util.Locale;
+
 /**
  *
  * @author reroes
@@ -16,6 +19,40 @@ public class Problema2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        double aCuadrado;
+        double aTriangulo;
+        double aRectangulo;
+
+        System.out.println("Ingrese el numero 1 si desea calcular el area del "
+                + "cuadrado");
+        aCuadrado = entrada.nextDouble();
+        System.out.println("Ingrese el numero 1 si desea calcular el area del "
+                + "triangulo");
+        aTriangulo = entrada.nextDouble();
+        System.out.println("Ingrese el numero 1 si desea calcular el area del "
+                + "rectangulo");
+        aRectangulo = entrada.nextDouble();
+
+        obtenerAreaCuadrado(aCuadrado);
+        obtenerAreaTriangulo(aTriangulo);
+        obtenerAreaRectangulo(aRectangulo);
     }
-    
+
+    public static double obtenerAreaCuadrado(double a) {
+        double operacion = a + a + a + a;
+        return operacion;
+    }
+
+    public static double obtenerAreaTriangulo(double b) {
+        double operacion = b * b / 2;
+        return operacion;
+    }
+
+    public static double obtenerAreaRectangulo(double c) {
+        double operacion = c * c;
+        return operacion;
+    }
 }
